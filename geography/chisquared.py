a2e7bd14e73a3a9ae30531f7c0ea66b8be006828
rpcrimi <rpcrimi@gmail.com>
2016-05-24 15:01:52 -0600

    fixed chi squared test

--- geography/chisquared.py
+++ geography/chisquared.py
@@ -1,21 +1,35 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from pprint import pprint
 from scipy.stats import chisquare
 from csv import DictReader
+
+f, (ax1) = plt.subplots(1)
 
 with open("geo_information_final_cluster.csv") as datafile:
 	data = list(DictReader(datafile))
 
+table_dict = {}
 genre_ids = {}
 genre_id = 0
 for track in data:
-	if track["genre"] not in genre_ids.keys():
-		genre_ids[track["genre"]] = genre_id
+	genre = track["genre"].split("|")[0]
+	if genre not in table_dict.keys():
+		table_dict[genre] = {track["cluster"]: 0 for track in data}
+		genre_ids[genre] = genre_id
 		genre_id += 1
 
+for track in data:
+	genre = track["genre"].split("|")[0]
+	table_dict[genre][track["cluster"]] += 1
 
-genres = [genre_ids[track["genre"]] for track in data]
-clusters = [int(track["cluster"]) for track in data]
+table = []
+for genre in table_dict:
+	row = []
+	for cluster in table_dict[genre]:
+		row.append(table_dict[genre][cluster])
+	table.append(row)
 
-obs = np.array([genres, clusters]).T
+pprint(table)
 
-print(chisquare(obs))
+print(chisquare(table))
