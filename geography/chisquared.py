f4711bb402d68c6bd1a81b218e489d4c8fff7078
rpcrimi <rpcrimi@gmail.com>
2016-05-26 10:01:20 -0600

    added genre topic correlation

--- geography/chisquared.py
+++ geography/chisquared.py
@@ -6,22 +6,25 @@ from csv import DictReader
 
 f, (ax1) = plt.subplots(1)
 
-with open("geo_information_final_cluster.csv") as datafile:
+#with open("geo_information_final_cluster.csv") as datafile:
+with open("../../millionsongtestset/MSD_master_data.csv") as datafile:
 	data = list(DictReader(datafile))
 
 table_dict = {}
 genre_ids = {}
 genre_id = 0
 for track in data:
-	genre = track["genre"].split("|")[0]
+	#genre = track["genre"].split("|")[0]
+	genre = track["genre"]
 	if genre not in table_dict.keys():
-		table_dict[genre] = {track["cluster"]: 0 for track in data}
+		table_dict[genre] = {track["topic"]: 0 for track in data}
 		genre_ids[genre] = genre_id
 		genre_id += 1
 
 for track in data:
-	genre = track["genre"].split("|")[0]
-	table_dict[genre][track["cluster"]] += 1
+	#genre = track["genre"].split("|")[0]
+	genre = track["genre"]
+	table_dict[genre][track["topic"]] += 1
 
 table = []
 for genre in table_dict:
@@ -30,19 +33,21 @@ for genre in table_dict:
 		row.append(table_dict[genre][cluster])
 	table.append(row)
 
-'''
-with open("chisquared.csv", "w+") as outfile:
+
+with open("genre_topic.csv", "w+") as outfile:
 	outfile.write(",".join(table_dict.keys())+"\n")
 	for row in table:
 		rowString = ""
 		for column in row:
 			rowString += str(column/sum(row))+","		
 		outfile.write(rowString.rstrip(',')+"\n")
-'''	
 
-pprint(table)
 
+for row in table:
+	print(row)
+'''
 print("Chi Squared")
 print(chisquare(table))
 print("Pearson's Chi Squared")
 print(chi2_contingency(table))
+'''
